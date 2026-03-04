af2b3da1621423537ca8748dfaf5c67ea3e2c4be
AshrafulDev609 <grow.ashraful@gmail.com>
2026-03-04 16:18:23 +0600

    react starter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+
+  return (
+    <div className="text-center h-screen w-full flex items-center justify-center flex-col">
+      <h2 className="font-bold text-center text-5xl">React starter</h2>
+      <p>TailwindCSS + DaisyUI</p>
+    </div>
+  )
+}
+
+export default App
