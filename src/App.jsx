2bf314c638e9e681a878b1a09389a538cfdb6fc7
AshrafulDev609 <grow.ashraful@gmail.com>
2026-03-05 23:42:35 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CustomerTickets from "./components/CustomerTickets/CustomerTickets"
 import Navbar from "./components/Navbar/Navbar"
 import TicketProgress from "./components/TicketProgress/TicketProgress"
 import { toast } from "react-toastify";
+import Footer from "./components/Footer/Footer";
 
 const ticketsPromise = fetch('data.json').then(res => res.json());
 
@@ -30,17 +31,21 @@ function App() {
   
 
   return (
-    <div className="md:w-[1200px] mx-auto">
-      <Navbar />
-      <Banner pendingTasks={pendingTasks} resolvedTasks={resolvedTasks} />
+    <>
+      <div className="md:w-[1200px] mx-auto">
+        <Navbar />
+        <Banner pendingTasks={pendingTasks} resolvedTasks={resolvedTasks} />
 
-      <section className="flex gap-5">
-        <Suspense fallback={<h2>Loading...</h2>}>
-          <CustomerTickets  handleAddTask={handleAddTask} ticketsPromise={ticketsPromise} />
-        </Suspense>
-        <TicketProgress pendingTasks={pendingTasks} handleCompletedTask={handleCompletedTask} resolvedTasks={resolvedTasks} />
-      </section>
-    </div>
+        <section className="flex gap-5 mb-10">
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <CustomerTickets  handleAddTask={handleAddTask} ticketsPromise={ticketsPromise} />
+          </Suspense>
+          <TicketProgress pendingTasks={pendingTasks} handleCompletedTask={handleCompletedTask} resolvedTasks={resolvedTasks} />
+        </section>
+      </div>
+
+      <Footer />
+    </>
   )
 }
 
